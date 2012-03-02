87dc12abcd1b6bb39d721d4f529d95ee94bd63a6
Mike Phillips <msphil@gmail.com>
2012-03-02 14:24:35 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roshambo::Application.config.secret_token = '60bc1ca28437b85e591c0d841f1eabb0bf6b4f0a487de36275524efd01f85bdb1b86bab483d8709aebac1205e98e81ccca1c4c97ed3b4289a9154d4ec4a3a870'
