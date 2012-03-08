b6ea950081a48623487a2578612e6f7c0164f919
Mike Phillips <msphil@gmail.com>
2012-03-07 19:21:15 -0800

    Update tests to test body on individual plays

--- spec/controllers/throw_controller_spec.rb
+++ spec/controllers/throw_controller_spec.rb
@@ -55,6 +55,89 @@ describe ThrowController do
 
   end
 
+  describe "Play a predictable losing game (check header)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'rock'
+      # Based on a test run, this results in a predictable game
+      response.should have_selector("h1", :content => "Rock!")
+    end
+  end
+
+  describe "Play a predictable losing game (check body)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'rock'
+      # Based on a test run, this results in a predictable game
+      response.should have_selector("p", :content => "Ouch")
+    end
+  end
+
+  describe "Play a predictable losing game (check session)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'rock'
+      # Based on a test run, this results in a predictable game
+      expected_wins = { computer: 1, player: 0, ties: 0 }
+      wins = session[:wins]
+      wins.should == expected_wins
+    end
+  end
+
+  describe "Play a predictable winning game (check session)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'scissors'
+      # Based on a test run, this results in a predictable game
+      expected_wins = { computer: 0, player: 1, ties: 0 }
+      wins = session[:wins]
+      wins.should == expected_wins
+    end
+  end
+
+  describe "Play a predictable winning game (check body)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'scissors'
+      # Based on a test run, this results in a predictable game
+      response.should have_selector("p", :content => "Nicely done")
+    end
+  end
+
+  describe "Play a predictable tie game (check session)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'paper'
+      # Based on a test run, this results in a predictable game
+      expected_wins = { computer: 0, player: 0, ties: 1 }
+      wins = session[:wins]
+      wins.should == expected_wins
+    end
+  end
+
+  describe "Play a predictable tie game (check body)" do
+    it "should be successful" do
+      # Make the PRNG predictable
+      # First play is "paper"
+      srand(0)
+      get 'paper'
+      # Based on a test run, this results in a predictable game
+      response.should have_selector("p", :content => "You tied")
+    end
+  end
+
   describe "Play a few games" do
     it "should be successful" do
       # Make the PRNG predictable
